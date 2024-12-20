267d9eac0075219a6a0fd232572d75ace1783084
Gonzalo Flores <qfbgonzaloflores96@hotmail.com>
2024-12-19 20:54:33 -0600

    Creanto repositorio en Github y vinculandolo con Git

---
+++ src/main/java/com/biblioGAFVi/BiblioGAFVi/BiblioGafViApplication.java
@@ -0,0 +1,13 @@
+package com.biblioGAFVi.BiblioGAFVi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioGafViApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioGafViApplication.class, args);
+	}
+
+}
