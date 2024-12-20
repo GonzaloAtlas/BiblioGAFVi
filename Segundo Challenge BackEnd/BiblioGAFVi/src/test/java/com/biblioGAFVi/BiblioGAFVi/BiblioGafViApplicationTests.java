469c3bea106d9860cb757249adab44f6867696d0
GonzaloAtlas <qfbgonzaloflores96@hotmail.com>
2024-12-19 20:26:51 -0600

    Add files via upload

---
+++ Segundo Challenge BackEnd/BiblioGAFVi/src/test/java/com/biblioGAFVi/BiblioGAFVi/BiblioGafViApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioGAFVi.BiblioGAFVi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioGafViApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
