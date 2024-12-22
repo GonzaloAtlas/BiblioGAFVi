6ef4eda74886d79dc4465448bcde6265872b1e5b
Gonzalo Flores <qfbgonzaloflores96@hotmail.com>
2024-12-21 22:14:00 -0600

    Creando interfases para crear metodos de busqueda JPA

--- src/main/java/com/biblioGAFVi/BiblioGAFVi/Repository/IAutoresRepository.java
+++ src/main/java/com/biblioGAFVi/BiblioGAFVi/Repository/IAutoresRepository.java
@@ -1,4 +1,11 @@
 package com.biblioGAFVi.BiblioGAFVi.Repository;
 
-public interface IAutoresRepository {
+import com.biblioGAFVi.BiblioGAFVi.Model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IAutoresRepository extends JpaRepository<Autor,Long> {
+    Autor findBynombreAutorIgnoreCase(String nombreAutor);
+    List<Autor> findByFechaDeNacimientoLessThanEqualAndFechaFallecimientoGreaterThanEqual(int anoInicial, int anoFinal);
 }
--- src/main/java/com/biblioGAFVi/BiblioGAFVi/Repository/ILibroRepository.java
+++ src/main/java/com/biblioGAFVi/BiblioGAFVi/Repository/ILibroRepository.java
@@ -1,4 +1,14 @@
 package com.biblioGAFVi.BiblioGAFVi.Repository;
 
-public interface ILibroRepository {
+import com.biblioGAFVi.BiblioGAFVi.Model.Autor;
+import com.biblioGAFVi.BiblioGAFVi.Model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ILibroRepository extends JpaRepository<Libro, Long> {
+    Optional<Libro> findByTituloIgnoreCase(String titulo);
+    List<Libro> findByIdiomaContaining(String idiomas);
+    List<Libro> findByAutor(Autor autor);
 }
